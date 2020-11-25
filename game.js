92909cfbb3faf9109a9546b84ff32354a37a0960
Andrei <andreifrolov83@gmail.com>
2020-11-25 10:30:30 +0300

    update game

--- game.js
+++ game.js
@@ -3,23 +3,40 @@ class Game {
     constructor() {
         this.interval = null;
         this.timeout = null;
+        this.container = document.getElementById("container");
+        this.ctx = this.container.getContext('2d');
+        this.fieldView = new FieldView(this.container);
+        this.fieldModel = new FieldModel(this.fieldView);
+        this.fieldController = new FieldController(this.fieldModel, this.fieldView);
+
+        this.goalKeeperView = new PlayerView(this.container, "goalKeeper");
+        this.goalKeeperModel = new PlayerModel(this.goalKeeperView);
+        this.goalKeeperController = new PlayerController(this.goalKeeperModel, this.goalKeeperView);
+
+        this.playerView = new PlayerView(this.container, "player");
+        this.playerModel = new PlayerModel(this.playerView);
+        this.playerController = new PlayerController(this.playerModel, this.playerView);
+
+        this.ballView = new BallView(this.container);
+        this.ballModel = new BallModel(this.ballView);
+        this.ballController = new BallController(this.ballModel, this.ballView);
     }
 
     regularState = function () {
-        ctx.clearRect(0, 0, container.width, container.height);
-        fieldController.run();
-        goalKeeperController.run();
-        playerController.run();
-        ballController.run();
+        this.ctx.clearRect(0, 0, this.container.width, this.container.height);
+        this.fieldController.run();
+        this.goalKeeperController.run();
+        this.playerController.run();
+        this.ballController.run();
     }
 
     goalStage = function () {
         var self = this;
         this.regularState();
         self.interval  = setInterval(() => {
-            fieldController.model.goalStage();
-            goalKeeperController.model.goalStage();
-            playerController.model.goalStage();
+            this.fieldController.model.goalStage();
+            this.goalKeeperController.model.goalStage();
+            this.playerController.model.goalStage();
         }, 150)
         this.timeout = setTimeout(() => {
             clearInterval(self.interval);
@@ -31,23 +48,6 @@ class Game {
         clearTimeout(this.timeout);
     }
 }
-var container = document.getElementById("container");
-var ctx = container.getContext('2d');
-var fieldView = new FieldView(container);
-var fieldModel = new FieldModel(fieldView);
-var fieldController = new FieldController(fieldModel, fieldView);
-
-var goalKeeperView = new PlayerView(container, "goalKeeper");
-var goalKeeperModel = new PlayerModel(goalKeeperView);
-var goalKeeperController = new PlayerController(goalKeeperModel, goalKeeperView);
-
-var playerView = new PlayerView(container, "player");
-var playerModel = new PlayerModel(playerView);
-var playerController = new PlayerController(playerModel, playerView);
-
-var ballView = new BallView(container);
-var ballModel = new BallModel(ballView);
-var ballController = new BallController(ballModel, ballView);
 
 var g = new Game();
 g.regularState();
