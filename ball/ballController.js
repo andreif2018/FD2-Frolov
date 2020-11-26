27fd6c025cc1ef00351a5f4b84351b14c8a0e1fa
Andrei <andreifrolov83@gmail.com>
2020-11-26 16:41:54 +0300

    ball flow in target and kick together

--- ball/ballController.js
+++ ball/ballController.js
@@ -4,6 +4,8 @@ class BallController {
     constructor(model, view) {
         this.model = model;
         this.view = view;
+        document.addEventListener('keydown', this.keyDownHandler, false);
+        document.addEventListener('keyup', this.keyUpHandler, false);
     }
 
     start = function () {
@@ -16,5 +18,16 @@ class BallController {
 
     goalStage = function () {
         this.model.goalStage();
+    }
+
+    keyDownHandler = function (event) {
+        // if (event.key === 'ArrowUp') rightRacquetH.speedY = -3;
+        // else if (event.key === 'ArrowDown') rightRacquetH.speedY = 3;
+    }
+
+    keyUpHandler = function (event) {
+        if (event.key === 'Space') console.log("space");
+        // if (event.key === 'ArrowUp') rightRacquetH.speedY = 0;
+        // else if (event.key === 'ArrowDown') rightRacquetH.speedY = 0;
     }
 }
